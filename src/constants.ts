acda5bb0b4a700b5c5528a2909e80b5f7a962962
gabrielpersonalprojects <gabrielpersonalproject1@gmail.com>
2026-01-11 15:11:19 -0300

    fix constants keys

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,21 @@
-export const CATEGORIAS_PADRAO = [];
-
+export const CATEGORIAS_PADRAO = {
+  despesa: [
+    "Alimentação",
+    "Transporte",
+    "Moradia",
+    "Saúde",
+    "Lazer",
+    "Educação",
+    "Assinaturas",
+    "Compras",
+    "Contas",
+    "Outros"
+  ],
+  receita: [
+    "Salário",
+    "Freelance",
+    "Vendas",
+    "Investimentos",
+    "Outros"
+  ]
+};

619c9ef796fe2be8bd174c180de0bb2fe731dac4
gabrielpersonalprojects <gabrielpersonalproject1@gmail.com>
2026-01-11 16:57:26 -0300

    Wrap App with UIProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { UIProvider } from "./components/UIProvider";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <UIProvider>
     <App />
+      </UIProvider>
   </React.StrictMode>
 )
 

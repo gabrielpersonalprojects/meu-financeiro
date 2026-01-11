25eaf916fc9e08e9471186dd81389c879f26f4cb
gabrielpersonalprojects <gabrielpersonalproject1@gmail.com>
2026-01-11 15:15:13 -0300

    fix types

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export type TransactionType = "despesa" | "receita";
+export type SpendingType = "Fixo" | "Variável";
+export type TabType = "transacoes" | "gastos" | "projecao";
+
+export type PaymentMethod = "" | "pix" | "credito_vista" | "debito" | "boleto";
+
+export type Categories = {
+  despesa: string[];
+  receita: string[];
+};
+
+export type PaymentMethods = {
+  credito: string[];
+  debito: string[];
+};
+
+export interface Transaction {
+  id: number;
+  tipo: TransactionType;
+  descricao: string;
+  valor: number;
+  data: string; // YYYY-MM-DD
+  categoria: string;
+  tipoGasto: SpendingType | "";
+  metodoPagamento: PaymentMethod;
+  qualCartao: string;
+  pago: boolean;
+
+  // opcionais
+  recorrenciaId?: string;
+  isRecorrente?: boolean;
+}
